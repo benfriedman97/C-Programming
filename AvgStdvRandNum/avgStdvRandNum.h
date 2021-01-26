11cf4942ad8e513005ff7399198dcfd8298cb046
Ben Friedman <55304085+benfriedman97@users.noreply.github.com>
2021-01-25 19:15:46 -0500

    Update avgStdvRandNum.h

--- AvgStdvRandNum/avgStdvRandNum.h
+++ AvgStdvRandNum/avgStdvRandNum.h
@@ -5,23 +5,19 @@
 
 typedef void* AVG_STDV;
 
-/*Precondition: none
-Postcondition: returns handle to a valid avg_and_stdv object.
-avg and stdv within the object are intialized to 0*/
+/* Precondition: none
+   Postcondition: returns handle to a valid avg_and_stdv object.
+   avg and stdv within the object are intialized to 0 */
 AVG_STDV avg_and_stdv_init_default(void);
 
-//For 3.2
-/*Precondition: hAvg_and_stdv is a valid handle to an avg_and_stdv object
-Postcondition: the average and standard deviation of a set of N numbers, where
-each number is randomly generated and is no greater than r - 1, is now stored
-in the object pointed to by the handle*/
+/* Precondition: hAvg_and_stdv is a valid handle to an avg_and_stdv object
+   Postcondition:  */
 Status avg_and_stdv_calculate_avg_and_stdv1(AVG_STDV hAvg_and_stdv, int N, int r);
 
-//For 3.3
-/*Precondition: hAvg_and_stdv is a valid handle to an avg_and_stdv object
-Postcondition: the average and standard deviation of a set of N numbers, where
-each number is randomly generated and is no greater than r - 1, is now stored
-in the object pointed to by the handle*/
+/* Precondition: hAvg_and_stdv is a valid handle to an avg_and_stdv object
+    Postcondition: the average and standard deviation of a set of N numbers, where
+    each number is randomly generated and is no greater than r - 1, is now stored
+    in the object pointed to by the handle */
 Status avg_and_stdv_calculate_avg_and_stdv2(AVG_STDV hAvg_and_stdv, int N, int r);
 
 /*Precondition: hAvg_and_stdv is a valid object
