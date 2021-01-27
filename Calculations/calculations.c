a46193e0d5983fb2a96b41d2121c670921dba277
Ben Friedman <55304085+benfriedman97@users.noreply.github.com>
2021-01-26 21:12:42 -0500

    Update calculations.c

--- Calculations/calculations.c
+++ Calculations/calculations.c
@@ -7,18 +7,18 @@
 
 /* Approximates value of harmonic function using formula
    H = ln(N) + y + 1/(12N) where y is euler's constant (0.57721...) */
-long double harmonic_nat_log(int n);
+long double harmonic_nat_log(unsigned n);
 
-/* Calculates the harmonic sigma function for up to N */
-long double harmonic_sigma(int n);
+/* Calculates the harmonic function for up to N. This is one not an approximation */
+long double harmonic_sigma(unsigned n);
 
-/* Calculates lg lg N-- log_2(log_2 N) */
-int calculate(int n);
+/* Calculates ceiling of lg lg N...ceil(log_2(log_2(N))) without library math functions */
+unsigned calculate(unsigned n);
 
-/* Calculates the number of bits in lg(N!)
+/* Calculates the number of bits in teh binary representation of lg(N!)
    In general, ceiling(lg(N + 1)) is the number of 
    bits in N, so calcaulate do ceiling(lg(N! + 1)) */
-int calc_bits_in_lg_N_facotorial(int n);
+long unsigned calc_bits_in_lg_N_facotorial(long unsigned n);
 
 /* prints out the prime numbers up to N */
 void sieve_of_eratosthenes_original(int N);
@@ -33,11 +33,41 @@ int sieve_of_eratosthenes_num_primes(int N);
 
 
 
-long double harmonic_nat_log(int n) {
-	return log(n) + EULER + 1.0 / (12 * n);
+int main(int argc, char *argv[]) {
+	printf("CALCULATION 1\n");
+	printf("Implementing the functions to calculate the harmonic sigma function, as well as a function to\n"
+		"approximate it. As N increases, the approximation will become more accurate\n");
+	for (unsigned i = 1; i <= 9; i++) {
+		unsigned N = pow(10, i);
+		printf("Calculating the harmonic function with N = %d\n", N);
+		printf("Real value: %lf\tApproximation: %lf\n\n", harmonic_sigma(N), harmonic_nat_log(N));
+	}
+	printf("\n\n\n");
+	
+	printf("CALCULATION 2\n");
+	printf("Implementing the function to calculate ceil(lg(lg(N))) without built in library functions\n");
+	for (unsigned i = 1; i <= 9; i++) {
+		unsigned N = pow(10, i);
+		printf("lg(lg(%d)) = %d\n", N, calculate(N));
+	}
+	printf("\n\n\n");
+
+	printf("CALCULATION 3\n");
+	printf("Implementing the function to calculate the number of bits in the binary representation of lg(N!)\n");
+	for (unsigned i = 0; i <= 13; i++) {
+		printf("Number of bits in binary representation of lg(%d!) = %d\n", i, calc_bits_in_lg_N_facotorial(i));
+	}
+	printf("\n\n\n");
+	return 0;
 }
 
-long double harmonic_sigma(int n) {
+
+
+long double harmonic_nat_log(long unsigned n) {
+	return log(n) + EULER + 1.0 / ((long unsigned)12 * n);
+}
+
+long double harmonic_sigma(unsigned n) {
 	if (n < 1) {
 		printf("error. n must be at least 1\n");
 		return ERROR;
@@ -48,15 +78,13 @@ long double harmonic_sigma(int n) {
 	return harmonic;
 }
 
-
-
-int calculate(int n) {
+unsigned calculate(unsigned n) {
 	if (n <= 1) {
 		printf("Error. N must be at least 2\n");
 		return ERROR;
 	}
 
-	int lg1 = 0,		//will be highest whole integer power of 2 that is less than or equal to n
+	unsigned lg1 = 0,		//will be highest whole integer power of 2 that is less than or equal to n
 		remainder = 0;	//calculates remainder after each division of 2
 	while (n >= 2) {
 		remainder += n % 2;
@@ -71,7 +99,7 @@ int calculate(int n) {
 		return 1;
 
 	//check if 2 fits perfectly into lg1
-	int lg2; 
+	unsigned lg2; 
 	lg2 = remainder = 0;
 	while (lg1 > 1) {
 		remainder += lg1 % 2;
@@ -85,8 +113,8 @@ int calculate(int n) {
 }
 
 
-int calc_bits_in_lg_N_facotorial(int n) {
-	int factorial = 1,
+long unsigned calc_bits_in_lg_N_facotorial(long unsigned n) {
+	long unsigned factorial = 1,
 		num_bits = 1,	//even 0! = 1 which is 1 bit
 		remainder = 0;
 	//step 1: calculate factorial and add 1
@@ -94,7 +122,7 @@ int calc_bits_in_lg_N_facotorial(int n) {
 		factorial *= i;
 	factorial++;
 	//step 2: calculate ceiling lg(factorial)
-	int lg = 0;
+	long unsigned lg = 0;
 	while (factorial >= 2) {
 		remainder += factorial % 2;
 		factorial /= 2;
